52c1937f8117eaab02df956269007439e0f531eb
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2023-10-10 11:27:56 +0530

    Add files via upload

---
+++ areaandperiofsq.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int s;
+	float area,perimeter;
+	scanf("%d",&s);
+	area = s*s;
+	perimeter = 4*s;printf("area = %f \n perimeter = %f",area,perimeter);
+	return 0;
+}
