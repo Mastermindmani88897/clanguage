4f6728a5612a8040222e5dccc6b93b0db589ffad
Mastermindmani88897 <147472580+Mastermindmani88897@users.noreply.github.com>
2023-10-10 11:19:53 +0530

    Add files via upload

---
+++ all in one .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i=8,j=2,a,s,m,d;
+	a=i+j;
+	printf("sum of %d and %d  is %d\n",i,j,a);
+	 
+	s=i-j;
+	printf("sub of %d and %d is %d\n",i,j,s);
+	
+	d=i/j;
+	printf("div of %d and %d is %d\n",i,j,d);
+	
+	m=i*j;
+	printf("mul of %d and %d is %d\n",i,j,m);
+	
+	return 0;  
+	
+}
